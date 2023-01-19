d07df338100a6e27456e162ea930f3525a38a1fb
asterjody <116712028+asterjody@users.noreply.github.com>
2023-01-20 00:26:58 +0700

    3rde

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -13,7 +13,7 @@ const Skills = () => {
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
                             <Image 
-                                src='/../public/assets/skills/html.png'
+                                src='../public/assets/skills/html.png'
                                 width={64}
                                 height={64}
                                 alt='/' 
